ad0492ccfc46bb8620294f77fdaf17dbec303f54
Egor <seniorpetrenko@gmail.com>
2025-12-28 15:56:38 +0300

    Настройка url для API и удаление лишнего

--- src/main/java/ru/bmstu/api/controller/HelloController.java
+++ src/main/java/ru/bmstu/api/controller/HelloController.java
@@ -1,7 +1,6 @@
 package ru.bmstu.api.controller;
 
 import org.springframework.web.bind.annotation.*;
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin(origins = {
@@ -12,17 +11,17 @@ import jakarta.servlet.http.HttpServletResponse;
 public class HelloController {
 
     @GetMapping("/url1")
-    public String url1(HttpServletResponse response) {
+    public String url1() {
         return "Привет!";
     }
 
     @GetMapping("/url2")
-    public int url2(HttpServletResponse response) {
+    public int url2() {
         return 2025;
     }
 
     @GetMapping("/url3")
-    public Boolean url3(HttpServletResponse response) {
+    public Boolean url3() {
         return true;
     }
 }
