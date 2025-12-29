67881f222a97f544fe9f4a11de30d976a5411212
Egor <seniorpetrenko@gmail.com>
2025-12-29 14:07:01 +0300

    Создание юнит-тестов

--- src/main/java/ru/bmstu/api/controller/HelloController.java
+++ src/main/java/ru/bmstu/api/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = {
-        "http://192.168.56.1:3000"
+        "*"
 })
 
 @RequestMapping("/api")
--- src/test/java/ru/bmstu/api/ApiApplicationTests.java
+++ src/test/java/ru/bmstu/api/ApiApplicationTests.java
@@ -1,13 +1,44 @@
+// HelloControllerUnitTest.java
 package ru.bmstu.api;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.bmstu.api.controller.HelloController;
 
-@SpringBootTest
-class ApiApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-	@Test
-	void contextLoads() {
-	}
+@WebMvcTest(controllers = HelloController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+class HelloControllerUnitTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Тест /api/url1 на возврат \"Привет CI/CD!!!\"")
+    void testUrl1() throws Exception {
+        mockMvc.perform(get("/api/url1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Привет CI/CD!!!"));
+    }
+
+    @Test
+    @DisplayName("Тест /api/url2 на возврат числа 2025")
+    void testUrl2() throws Exception {
+        mockMvc.perform(get("/api/url2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("2025"));
+    }
+
+    @Test
+    @DisplayName("Тест /api/url3 на возврат true")
+    void testUrl3() throws Exception {
+        mockMvc.perform(get("/api/url3"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
 }
